6cb30db9e05a0d185510f83c3db8d5bc8220d883
STJ <ssongtj0505@gmail.com>
2024-02-05 16:06:35 +0900

    page , component 폴더 추가

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    1번째
+                </nav>
+                
+            </div>
+        );
+    }
+}
+
+export default Header;
