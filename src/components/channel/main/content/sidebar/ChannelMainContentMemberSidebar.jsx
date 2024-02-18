43d7276a27b33c15d7ef53537d9de9ac8596e542
cloudchamb3r <jizon0123@protonmail.com>
2024-02-18 22:06:46 +0900

    change channel member sidebar bgcolor

--- src/components/channel/main/content/sidebar/ChannelMainContentMemberSidebar.jsx
+++ src/components/channel/main/content/sidebar/ChannelMainContentMemberSidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 const ChannelMainContentMemberSidebarContainer = styled.div`
-    background-color: red;
+    background-color: #f2f3f5;
     width: 240px;
 `;
 const ChannelMainContentMemberSidebar = () => {
