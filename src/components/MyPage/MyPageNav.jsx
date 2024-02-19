21b5ef0222a9a4f106e73be7f781e18f5c46db17
ran <ran2rrrw@gmail.com>
2024-02-19 18:15:31 +0900

    mypage profile

--- src/components/MyPage/MyPageNav.jsx
+++ src/components/MyPage/MyPageNav.jsx
@@ -1,21 +1,25 @@
 import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 
-
-const MyPageNav = () => {
-    return(
+const MyPageNav = ({ showProfile, showChannelProfile }) => {
+    return (
         <List>
             <ListItem>
-                <ListItemButton>
-                    <ListItemText primary="내계정"/>
+                <ListItemButton onClick={showProfile}>
+                    <ListItemText primary="내계정" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton onClick={showChannelProfile}>
+                    <ListItemText primary="프로필" />
                 </ListItemButton>
             </ListItem>
             <ListItem>
                 <ListItemButton>
-                    <ListItemText primary="프로필"/>
+                    <ListItemText primary="로그아웃" />
                 </ListItemButton>
             </ListItem>
         </List>
     );
 };
-export {MyPageNav};
+export { MyPageNav };
--- src/components/MyPage/MyPageProfile.jsx
+++ src/components/MyPage/MyPageProfile.jsx
@@ -1,62 +1,91 @@
-import { BaseContainer } from "@components/basis/BaseContainer";
 import { MemberContext } from "@contexts/MemberContext";
 import styled from "@emotion/styled";
-import { Avatar, Box, Container } from "@mui/material";
+import { Avatar, Box, Button, Container } from "@mui/material";
 import { useContext } from "react";
 
-const MyPageProfileContainer = styled(BaseContainer)`
+const MyPageProfileContainer = styled.div`
+    width: 660px;
+    height: 100vh;
+    padding: 60px 40px 80px 40px;
     display: flex;
     flex-direction: column;
 `;
 
-const labelContainer = styled.div`
-    width: 100px;
+const UserInfo = styled.div`
+  width: 550px;
+  height: 72px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 12px 16px 12px;
 `;
 
-const contentContainer = styled.div`
-    width:100px;
+const ProfileContainer = styled.div`
+    margin-top: 10px;
+    margin-bottom: 24px;
+`;
+
+const LabelContainer = styled.div`
+    width: 520px;
+    font-size: 12px;
+    color: "#b5bac1";
+`;
+
+const ContentContainer = styled.div`
+    width: 520px;
+    font-size: 16px;
+    color: "#f2f3f5";
 `;
 
 
 
-const MyPageProfile = () => {
+const MyPageProfile = ({ showChannelProfile }) => {
 
-    const {nickname, email} = useContext(MemberContext);
+    const { nickname, email } = useContext(MemberContext);
 
-    return(
+    return (
         <MyPageProfileContainer>
             <h3>내계정</h3>
             <Container fixed sx={{
-                bgcolor:"#c3c1c1ede",
-                borderRadius: 2
+                bgcolor: "#dedede",
+                borderRadius: 2,
+                padding: 3
             }}>
-                <Avatar sx={{
-                    width: 30,
-                    height: 30
-                }}
-                >
-                    T
-                </Avatar>
-
+                <UserInfo>
+                    <Avatar sx={{
+                        width: 80,
+                        height: 80,
+                        border: "3px solid rgba(0, 0, 0, 0.54)"
+                    }}
+                    >
+                        T
+                    </Avatar>
+                    <div style={{ width: 300, fontSize: 20 }}>{nickname}</div>
+                    <Button variant="contained" size="medium" sx={{ height: 35 }} onClick={showChannelProfile}>
+                        프로필 편집
+                    </Button>
+                </UserInfo>
                 <Box sx={{
                     bgcolor: "#ebedef",
                     borderRadius: 2,
+                    margin: 2,
+                    padding: 2,
                     display: "flex",
-                    flexDirection: "column"
+                    flexDirection: "column",
                 }}>
-                    <div>
-                        <labelContainer>별명</labelContainer>
-                        <contentContainer>{nickname}</contentContainer>
-                    </div>
-                    <div>
-                        <labelContainer>이메일</labelContainer>
-                        <contentContainer>{email}</contentContainer>
-                    </div>
+                    <ProfileContainer>
+                        <LabelContainer>별명</LabelContainer>
+                        <ContentContainer>{nickname}</ContentContainer>
+                    </ProfileContainer>
+                    <ProfileContainer>
+                        <LabelContainer>이메일</LabelContainer>
+                        <ContentContainer>{email}</ContentContainer>
+                    </ProfileContainer>
                 </Box>
-                
+
             </Container>
         </MyPageProfileContainer>
 
     );
 };
-export {MyPageProfile};
+export { MyPageProfile };
