17ab5fe42d40cb01c24e3875b2156241aa917f0e
STJ <ssongtj0505@gmail.com>
2024-02-22 11:37:11 +0900

    chat emoji 추가

--- src/components/channel/main/content/chat/ChannelMainContentChatItem.jsx
+++ src/components/channel/main/content/chat/ChannelMainContentChatItem.jsx
@@ -1,20 +1,75 @@
-import { ListItem } from "@mui/material";
+import axiosClient from "@components/AxiosClient";
+import styled from "@emotion/styled";
+import { Avatar, ListItem } from "@mui/material";
+import dayjs from "dayjs";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import { useParams } from "react-router-dom";
+import remarkGfm from "remark-gfm";
+
+const AvatarView = styled.div`
+    width: 48px;
+`;
+
+const ContentView = styled.div`
+    
+`;
+
+const AuthorInfo = styled.div`
+    display: flex;
+`;
+
+const ContentMarkdown = styled(Markdown)`
+    & p {
+        line-height: 1;
+    }
+`;
+
 
 const ChannelMainContentChatItem = ({
     msg,
     showProfile
 }) => {
+    const [content, setContent] = useState([]);
+    const { channelId, topicId } = useParams();
+
+    useEffect(() => {
+        getContent();
+    }, []);
+
+    const getContent = async () => {
+        // const data = await axiosClient.get("https://diverse-mule-possible.ngrok-free.app/api/channel/" + channelId + "/topic/" + topicId + "/message");
+        // setContent(data);
+        // console.log(content);
+        const chatList = await axiosClient.get("https://diverse-mule-possible.ngrok-free.app/api/channel/" + channelId + "/topic/" + topicId + "/message");
+        setContent(chatList.data);
+        console.log(content);
+    };
+
+    const listStyle = showProfile
+        ? { alignItems: "normal", display: "flex", padding: "20px 16px 0" }
+        : { alignItems: "normal", display: "flex", padding: "0 16px" };
+
     return (
-        <ListItem sx={{ flexDirection: "column", alignItems: "normal" }}>
-            {showProfile && (
-                <div>
-                    <h4>{msg.author}</h4>
-                </div>
-            )}
-            <div>
-                {msg.content}
-            </div>
+        <ListItem sx={listStyle}>
+            <AvatarView >
+                {showProfile && <Avatar />}
+            </AvatarView>
+
+            <ContentView>
+                {showProfile && <AuthorInfo>
+                    <div style={{ fontWeight: 700, paddingRight: "8px" }}>{msg.author}</div>
+                    <div>{dayjs(msg.date).format("YYYY-MM-DD HH:mm:ss")}</div>
+                </AuthorInfo>}
+                <ContentMarkdown remarkPlugins={[remarkGfm]}>
+                    {/* {content.map((item, idx) =>
+                        <div key={idx}>
+                            <div>{item}</div>
+                        </div>
+                    )} */}
+                </ContentMarkdown>
+            </ContentView>
         </ListItem>
     );
 };
--- src/components/channel/main/content/sidebar/ChannelMainContentMemberSidebar.jsx
+++ src/components/channel/main/content/sidebar/ChannelMainContentMemberSidebar.jsx
@@ -1,15 +1,49 @@
 import styled from "@emotion/styled";
+import { Avatar, ListItem } from "@mui/material";
+import { PropTypes } from "prop-types";
 
 const ChannelMainContentMemberSidebarContainer = styled.div`
     background-color: #f2f3f5;
     width: 240px;
 `;
-const ChannelMainContentMemberSidebar = () => {
+
+const ContentView = styled.div`
+    
+`;
+
+const AuthorInfo = styled.div`
+    display: flex;
+`;
+
+const ChannelMainContentMemberSidebar = ({
+    msg,
+    showProfile
+}) => {
     return (
         <ChannelMainContentMemberSidebarContainer>
-            this is member side bar
+            <ListItem>
+                <Avatar>
+                    {showProfile && <Avatar />}
+                </Avatar>
+                <ContentView>
+                    {showProfile && <AuthorInfo>
+                        {msg.author}
+                    </AuthorInfo>}
+                </ContentView>
+            </ListItem>
+
+            <div>오프라인</div>
         </ChannelMainContentMemberSidebarContainer>
     );
+};
+
+ChannelMainContentMemberSidebar.propTypes = {
+    msg: PropTypes.shape({
+        author: PropTypes.string,
+        content: PropTypes.string,
+        date: PropTypes.number,
+    }).isRequired,
+    showProfile: PropTypes.bool,
 };
 
 export { ChannelMainContentMemberSidebar };
