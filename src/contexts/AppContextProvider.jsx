8cdabd6ed86568b7884f425de7d21db786414bea
cloudchamb3r <jizon0123@protonmail.com>
2024-02-28 14:44:33 +0900

    remove unused dependencies

--- src/contexts/AppContextProvider.jsx
+++ src/contexts/AppContextProvider.jsx
@@ -1,10 +1,8 @@
-import { getGoogleOAuth2ClientId } from "@configs/env";
 import { theme } from "@configs/theme";
 import { ChannelContextProvider } from "@contexts/ChannelContext";
 import { MemberContextProvider } from "@contexts/MemberContext";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import PropTypes from "prop-types";
 
 const AppContextProvider = ({ children }) => {
