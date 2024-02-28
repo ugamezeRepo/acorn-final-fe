36d72513f2e9da184389826d30ef760f9f039ee9
cloudchamb3r <jizon0123@protonmail.com>
2024-02-28 11:18:04 +0900

    working on oauth2

--- src/contexts/AppContextProvider.jsx
+++ src/contexts/AppContextProvider.jsx
@@ -1,20 +1,24 @@
+import { getGoogleOAuth2ClientId } from "@configs/env";
 import { theme } from "@configs/theme";
 import { ChannelContextProvider } from "@contexts/ChannelContext";
 import { MemberContextProvider } from "@contexts/MemberContext";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import PropTypes from "prop-types";
 
 const AppContextProvider = ({ children }) => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <MemberContextProvider>
-                <ChannelContextProvider>
-                    {children}
-                </ChannelContextProvider>
-            </MemberContextProvider>
-        </ThemeProvider>
+        <GoogleOAuthProvider clientId={getGoogleOAuth2ClientId()}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <MemberContextProvider>
+                    <ChannelContextProvider>
+                        {children}
+                    </ChannelContextProvider>
+                </MemberContextProvider>
+            </ThemeProvider>
+        </GoogleOAuthProvider>
     );
 };
 
