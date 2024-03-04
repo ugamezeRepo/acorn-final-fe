f3443925b9f55908a2c78b24fdcc51a719b75767
STJ <ssongtj0505@gmail.com>
2024-03-04 14:36:46 +0900

    Topic setting 진행 중

--- src/components/channel/nav/ChannelNavItem.jsx
+++ src/components/channel/nav/ChannelNavItem.jsx
@@ -1,8 +1,10 @@
 import { ChannelContext } from "@contexts/ChannelContext";
+import styled from "@emotion/styled";
 import { Tag } from "@mui/icons-material";
-import { ListItemButton } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { ListItemButton, Popover } from "@mui/material";
 import PropTypes from "prop-types";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -13,13 +15,35 @@ const ChannelNavItem = ({
 }) => {
     const { currentChannel } = useContext(ChannelContext);
     const navigate = useNavigate();
+    const [openSet, SetOpenSet] = useState(null);
+    const openTopicSetting = Boolean(openSet);
+    const OpenTopicSet = (e) => {
+        SetOpenSet(e.currentTarget);
+    };
+    const CloseTopicSetting = () => {
+        SetOpenSet(null);
+    };
+
+    const SetIcon = styled(SettingsIcon)`
+        color: #cacaca;
+        &:hover {
+            color:#808080;
+        }
+    `;
+
     return (
-        <ListItemButton onClick={() => {
-            navigate(`/channel/${currentChannel.id}/topic/${topicId}`);
-        }} sx={{ display: "flex", margin: "6px", borderRadius: "8px" }}>
-            <Tag fontSize="small" sx={{ color: "gray", marginRight: "6px" }} />
-            <span style={{ fontSize: "0.9em" }}>{topicName}</span>
-        </ListItemButton>
+        <>
+            <ListItemButton onClick={() => {
+                navigate(`/channel/${currentChannel.id}/topic/${topicId}`);
+            }} sx={{ display: "flex", justifyContent: "space-between", margin: "6px", borderRadius: "8px" }}>
+                <div style={{ display: "flex" }}>
+                    <Tag fontSize="small" sx={{ color: "gray", marginRight: "6px" }} />
+                    <span style={{ fontSize: "0.9em" }}>{topicName}</span>
+                </div>
+                <SetIcon fontSize="small" onClick={OpenTopicSet} />
+            </ListItemButton>
+            <Popover open={openTopicSetting} onClose={CloseTopicSetting} />
+        </>
     );
 };
 
