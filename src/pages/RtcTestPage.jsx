947624277e81e6b3e8ad69714ada360e0abf1453
cloudchamb3r <jizon0123@protonmail.com>
2024-03-17 15:36:15 +0900

    fix ice server settings...

--- src/pages/RtcTestPage.jsx
+++ src/pages/RtcTestPage.jsx
@@ -54,13 +54,8 @@ const RtcTestPage = () => {
     useEffect(() => {
         (async () => {
             const rtc = new RTCPeerConnection({
-                iceServers: [
-                    { urls: "stun:stun.l.google.com:19302" },
-                    { urls: "stun:stun1.l.google.com:19302" },
-                    { urls: "stun:stun2.l.google.com:19302" },
-                    { urls: "stun:stun3.l.google.com:19302" },
-                    { urls: "stun:stun4.l.google.com:19302" },
-                ]
+                iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+                iceTransportPolicy: "all",
             });
 
             rtc.onicecandidate = ({ candidate }) => {
