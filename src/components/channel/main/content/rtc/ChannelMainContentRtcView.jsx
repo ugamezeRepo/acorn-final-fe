c9c03376ff1c95c20df8bcdac5c6190ed92b58f9
cloudchamb3r <jizon0123@protonmail.com>
2024-03-19 18:32:02 +0900

    wip

--- src/components/channel/main/content/rtc/ChannelMainContentRtcView.jsx
+++ src/components/channel/main/content/rtc/ChannelMainContentRtcView.jsx
@@ -28,7 +28,7 @@ const ChannelMainContentRtcView = () => {
     const remoteVideosRef = useRef({});
     const { currentChannel, currentTopic } = useContext(ChannelContext);
     const [signal, setSignal] = useState(null);
-    const { sendJsonMessage, lastJsonMessage } = useWebSocket(`${getWsBaseUrl()}/webrtc/channel/${currentChannel.id}/topic/${currentTopic.id}`);
+    const { sendJsonMessage, lastJsonMessage } = useWebSocket(`${getWsBaseUrl()}/webrtc/channel/${currentChannel.id}/topic/${currentTopic.id}`, { shouldReconnect: () => true });
     const [uuid] = useState(crypto.randomUUID());
     const [participants, setParticipants] = useState({});
 
@@ -37,7 +37,8 @@ const ChannelMainContentRtcView = () => {
             .getUserMedia({ video: true, audio: true })
             .then(stream => myVideoRef.current.srcObject = stream);
         setSignal({ uuid });
-    }, []);
+        console.log("uuid changed");
+    }, [uuid]);
 
     useEffect(() => {
         if (!signal) return;
@@ -48,29 +49,36 @@ const ChannelMainContentRtcView = () => {
         function createNewPeerConnection(remoteUuid) {
             const rtc = new RTCPeerConnection(rtcConfig);
             rtc.onicecandidate = ({ candidate }) => {
+                console.log("on ice candidate");
                 setSignal({ candidate, uuid });
             };
             rtc.onnegotiationneeded = async () => {
+                console.log("on negotiation needed");
                 const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-                stream.getTracks().forEach(t => participants[remoteUuid].pc.addTrack(t, stream));
-                const offer = await participants[remoteUuid].pc.createOffer();
-                await participants[remoteUuid].pc.setLocalDescription(offer);
+                stream.getTracks().forEach(t => rtc.addTrack(t, stream));
+                const offer = await rtc.createOffer();
+                await rtc.setLocalDescription(offer);
                 setSignal({ desc: offer, uuid });
             };
             rtc.ontrack = (e) => {
+                console.log("on track");
                 remoteVideosRef.current[remoteUuid].srcObject = e.streams[0];
             };
+
+            console.log("create new peer connection called");
+            console.log(" created result => " + JSON.stringify({ uuid: remoteUuid, pc: rtc }));
             return { uuid: remoteUuid, pc: rtc };
         }
 
         if (!lastJsonMessage) return;
         const { desc, candidate, uuid: remoteUuid } = lastJsonMessage;
-        if (remoteUuid == uuid) return;
+        if (remoteUuid === uuid) return;
         let participant = participants[remoteUuid] ? participants[remoteUuid] : createNewPeerConnection(remoteUuid);
         let remove = false;
         (async () => {
             if (desc) {
                 if (desc.type === "offer") {
+                    console.log("[offer]");
                     await participant.pc.setRemoteDescription(desc);
                     const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
                     stream.getTracks().forEach(track => participant.pc.addTrack(track, stream));
@@ -79,13 +87,16 @@ const ChannelMainContentRtcView = () => {
                     setSignal({ desc: answer, uuid });
                 }
                 if (desc.type === "answer") {
+                    console.log("[answer]");
                     await participant.pc.setRemoteDescription(desc);
                 }
                 if (desc.type === "remove") {
+                    console.log("[remove] " + remoteUuid);
                     remove = true;
                 }
             }
             if (candidate) {
+                console.log("[candidate]");
                 await participant.pc.addIceCandidate(candidate);
             }
         })();
@@ -95,8 +106,13 @@ const ChannelMainContentRtcView = () => {
             }
             return { ...p, [remoteUuid]: participant };
         });
+        /** adding participants into dependency array makes infinite loop ;( */
+        // eslint-disable-next-line react-hooks/exhaustive-deps 
     }, [lastJsonMessage, uuid]);
 
+    useEffect(() => {
+        console.log(JSON.stringify(participants));
+    }, [participants]);
     return (
         <ChannelMainContentRtcViewContainer>
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ padding: "12px", overflow: "auto" }}>
